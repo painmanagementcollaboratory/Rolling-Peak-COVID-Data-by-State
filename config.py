839fe68dd256b062af33fcf022dcedccf3ef0b7b
bcoleman <drbccoleman@gmail.com>
2020-06-09 08:54:56 -0400

    06-09-2020 data update

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 config = {
-    'HOME_DIRECTORY' : '/Users/bcoleman/PycharmProjects/Rolling-Peak-COVID-Deaths-by-State'
+    'HOME_DIRECTORY' : '/Users/bcoleman/PycharmProjects/Rolling-Peak-COVID-Data-by-State'
 }
