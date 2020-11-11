7274dfe8640740572f2f954ec28c00490213ca6c
painmanagementcollaboratory <70152115+painmanagementcollaboratory@users.noreply.github.com>
2020-11-11 08:31:12 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,3 @@ import os
 config = {
     'HOME_DIRECTORY' : os.getcwd(),
 }
-
-
-'''config = {
-    'HOME_DIRECTORY' : '/Users/bcoleman/PycharmProjects/PMC-Rolling-Peak-COVID-Data-by-State'
-}
-'''
